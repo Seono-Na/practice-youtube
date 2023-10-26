0a69fb2bbdcbbef253b1c0f3feb650741971717d
Seono-Na <seono5670@gmail.com>
2023-10-26 20:29:48 +0900

    Router Settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import SearchHeader from "./components/SearchHeader";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <SearchHeader />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
