7d3d711e8def1ee8189c2645b35e72f41809238b
Seono-Na <seono5670@gmail.com>
2023-10-29 03:12:01 +0900

    검색헤더 기능

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { BsYoutube, BsSearch } from "react-icons/bs";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function SearchHeader() {
+  const { keyword } = useParams();
+  const navigate = useNavigate();
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+  useEffect(() => setText(keyword || ""), [keyword]);
   return (
     <div>
       <Link to="/">
         <BsYoutube />
         <h1>Youtube</h1>
       </Link>
-      <form>
-        <input />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search.."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <BsSearch />
       </form>
     </div>
